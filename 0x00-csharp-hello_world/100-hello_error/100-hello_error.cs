6759219ed4dee05242ae3c72cd47eb52fbc8a480
csarsound <csarsound@hotmail.com>
2022-12-12 20:50:21 -0500

    Hello, Error

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+        TextWriter errorWriter = Console.Error;
+        errorWriter.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+        System.Environment.Exit(1);
+    }
+}
