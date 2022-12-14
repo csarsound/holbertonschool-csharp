4cdfd4fbf86117ca6724a26554f22e86dffba0b4
csarsound <csarsound@hotmail.com>
2022-12-13 22:18:55 -0500

    AdogLove

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+
+public struct Dog
+{
+    public Dog(string _name, float _age, string _owner, Rating _rating)
+    {
+        name = _name;
+        age = _age;
+        owner = _owner;
+        rating = _rating;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public override string ToString() => $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+}
