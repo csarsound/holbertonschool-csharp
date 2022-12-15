c42e6608c89f63ae55b9c11772b20b386910b9bd
csarsound <csarsound@hotmail.com>
2022-12-14 20:31:41 -0500

    HealthValidation

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -4,6 +4,7 @@ namespace Enemies
 {
     /// <summary>
     /// This class creates a zombie.
+    /// </summary>
     public class Zombie
     {
         /// <summary>Field that defines the health of zombie.</summary> 
@@ -12,6 +13,16 @@ namespace Enemies
         public Zombie()
         {
             health = 0;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class and checks if value is greater than 0.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
         }
     }
 }
