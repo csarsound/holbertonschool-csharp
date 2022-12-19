4dbf7ca3e41ee2eaa6b89bf9d570942124d1d0d0
csarsound <csarsound@hotmail.com>
2022-12-19 18:22:30 -0500

    WhenI

--- 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -4,17 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random rndm = new Random();
-        int number = rndm.Next(-10000, 10000);
-        int lastDigit = number % 10;
-        string msg = "";
-        Console.Write($"The last digit of {number} is {lastDigit} ");
-        if (lastDigit > 5)
-            msg = "and is greater than 5";
-        else if (lastDigit == 0)
-            msg = "and is 0";
-        else if (lastDigit < 6)
-            msg = "and is less than 6 and not 0";
-        Console.WriteLine(msg);
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            if (letter != 'e' && letter != 'q')
+            {
+                Console.Write(letter);
+            }
+        }
     }
 }
