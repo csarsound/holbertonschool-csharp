d56d0cf89bc46c3101f253f8c40b55031d39fa3a
csarsound <csarsound@hotmail.com>
2022-12-19 18:48:43 -0500

    00..99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int number = 0; number <= 99; number++)
+        {
+            if (number != 99)
+            Console.Write($"{number:00}, ");
+            
+            else
+                Console.Write("{0}\n",number);
+        }
+
+    }
+}
