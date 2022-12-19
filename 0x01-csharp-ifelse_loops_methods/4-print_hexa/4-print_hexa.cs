64a0dd0db16c2a0600241970b7cd912f0f2817a2
csarsound <csarsound@hotmail.com>
2022-12-19 18:38:02 -0500

    HexadecimalPrinting

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int number = 0; number <= 98; number++)
+        {
+            Console.Write("{0} = {1}\n", number, String.Format("0x{0:x}", number));
+        }
+
+    }
+}
