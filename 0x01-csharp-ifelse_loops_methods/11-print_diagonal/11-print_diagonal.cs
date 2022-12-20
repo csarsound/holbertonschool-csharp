9310b29dc965ff4ec8ad150ab7396fa9ee5e4a69
csarsound <csarsound@hotmail.com>
2022-12-19 19:43:12 -0500

    ParallelUniverse

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+        {
+            for (int n = 0; n < length; n++)
+            {
+                Console.WriteLine("{0}\\", new string(' ', n));
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+    }
+}
