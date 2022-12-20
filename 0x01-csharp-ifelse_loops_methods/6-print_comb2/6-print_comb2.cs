c81ff8221919acf349c1ca5679d298b9da15735b
csarsound <csarsound@hotmail.com>
2022-12-19 19:00:50 -0500

    Brain&&Materials

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {        
+        for (int number = 0; number <= 9; number++)
+        {
+            for (int number1 = number + 1; number1 <= 9; number1++)
+            {
+                Console.Write("{0}{1}", number, number1);
+
+                if (!(number == 8 && number1 == 9))
+                {
+                    Console.Write(", ");
+                }
+            }                
+        }
+        Console.WriteLine();
+    }
+}
