fb20ef9eb552e0911c459ceaa31d1dc1e1402002
csarsound <csarsound@hotmail.com>
2022-12-19 20:19:45 -0500

    FizzBuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int n = 1; n <= 100; n++)
+        {
+            if (n % 3 == 0 && n % 5 == 0)
+            {
+                Console.Write("FizzBuzz");
+            }
+            else if (n % 3 == 0)
+            {
+                Console.Write("Fizz");
+            }
+            else if (n % 5 == 0)
+            {
+                Console.Write("Buzz");
+            }
+            else
+            {
+                Console.Write(n);
+            }
+            if (n != 100)
+            {
+                Console.Write(' ');
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
