62b85639a7ab45798d5f4fca86da0cbe49dfe4dd
csarsound <csarsound@hotmail.com>
2022-12-19 19:16:41 -0500

    There3Colors

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int number1 = number % 10;
+        
+        if (number1 < 0)
+        {
+            number1 = number1 * (-1);
+        }
+
+        Console.Write(number1);
+        return number1;
+    }
+}
