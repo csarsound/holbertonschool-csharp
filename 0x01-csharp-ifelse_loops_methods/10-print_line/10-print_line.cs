42497ae12330e0f69f82dc3e051b32ffaf15ed57
csarsound <csarsound@hotmail.com>
2022-12-19 19:35:08 -0500

    TheShortestDistance

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length > 0)
+        {
+            Console.WriteLine("{0}", new string('_', length));
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+    }
+}
