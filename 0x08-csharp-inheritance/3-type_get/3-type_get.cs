49832de5ca4e43d01bcc6da2fbabc8d478744150
csarsound <csarsound@hotmail.com>
2022-12-20 23:18:42 -0500

    Type

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+
+
+///<summary>Class that represents an object</summary>
+class Obj
+{
+    /// <summary>Checks if an obj is an instance of array</summary>
+    public static void Print(object myObj)
+    {
+
+
+        Type i = myObj.GetType();
+        TypeInfo j = i.GetTypeInfo();
+        IEnumerable<PropertyInfo> p_List = j.GetProperties();
+        IEnumerable<MethodInfo> my_List = j.GetMethods();
+        System.Console.WriteLine("{0} Properties:", j.Name);
+        
+        foreach (PropertyInfo pp in p_List)
+        {
+            
+            System.Console.WriteLine(pp.Name);
+        
+        }
+
+        System.Console.WriteLine("{0} Methods:", j.Name);
+        
+        foreach (MethodInfo mm in my_List)
+        {
+        
+            System.Console.WriteLine(mm.Name);
+        
+        }
+
+
+    }
+}
