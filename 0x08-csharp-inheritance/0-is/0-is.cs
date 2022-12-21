edbbff1b751d1296f3658973c3faa131a495d062
csarsound <csarsound@hotmail.com>
2022-12-20 22:55:28 -0500

    ThisIsOnefThese

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary>Class that represents an object</summary>
+class Obj
+{
+    /// <summary>Checks if an obj is an int</summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+            return (true);
+        else
+            return (false);
+    }
+}
