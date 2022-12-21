8927c24e41dc21b0f69917808e4fd5350c6b62da
csarsound <csarsound@hotmail.com>
2022-12-20 23:22:04 -0500

    ItsNotALesson

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>public class Animal</summary>
+public class Animal
+{
+
+}
+/// <summary>public derived class Dog</summary>
+public class Dog : Animal
+{
+
+}
