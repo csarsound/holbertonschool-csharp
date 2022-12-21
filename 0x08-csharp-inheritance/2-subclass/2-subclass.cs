dc61f53efd9b3759a55045e54a60a23ba5a8d9c0
csarsound <csarsound@hotmail.com>
2022-12-20 23:13:47 -0500

    OnlySubClass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+///<summary>Class that represents an object</summary>
+class Obj
+{
+    /// <summary>Checks if an obj is an instance of array</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+
+        
+        if (!(derivedType != baseType && derivedType.IsSubclassOf(baseType)))
+            return (false);
+        
+        else
+            return (true);
+    }
+}
