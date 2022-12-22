96cbc2313865c5ee2de060e51ff2afb9a909bc47
csarsound <csarsound@hotmail.com>
2022-12-21 23:08:31 -0500

    MultiplyBy2

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        var dicto = new Dictionary<string, int>();
+        foreach (KeyValuePair<string, int> item in myDict)
+        {
+            dicto.Add(item.Key, item.Value * 2);
+        }
+        return dicto;
+    }
+}
