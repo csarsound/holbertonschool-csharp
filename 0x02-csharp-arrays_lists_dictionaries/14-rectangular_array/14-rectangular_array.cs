4a43b28a907bb371f71dabeb9f8cd7dcab078734
csarsound <csarsound@hotmail.com>
2022-12-21 23:22:35 -0500

    RectangularArray

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[,] ar = new int[5, 5];
+		ar[2, 2] = 1;
+		
+		for (int a = 0; a < 5; a++)
+		{
+			Console.WriteLine("{0} {1} {2} {3} {4}", ar[a, 0], ar[a, 1], ar[a, 2], ar[a, 3], ar[a, 4]);
+		}
+	}
+}
