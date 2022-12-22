cd12ab4b3f222c98d340945bbc4ddc36654d49cd
csarsound <csarsound@hotmail.com>
2022-12-21 23:26:46 -0500

    MatrixScuared

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        for (int a = 0; a < myMatrix.GetLength(0); a++)
+        {
+            for (int b = 0; b < myMatrix.GetLength(1); b++)
+                myMatrix[a,b] = myMatrix[a,b] * myMatrix[a,b];
+        }
+        
+        return(myMatrix);
+    }
+}
