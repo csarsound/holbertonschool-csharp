83f8ae03286442974156143dd667ec1ac07c103a
csarsound <csarsound@hotmail.com>
2022-12-21 23:14:10 -0500

    PrintSortedDictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sorted = myDict.Keys.ToList();
+        
+        sorted.Sort();
+        foreach (var key in sorted)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        }
+    }
+}
