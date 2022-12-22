504bedb52104d6e824af9b91240a0f9ada8c7f94
csarsound <csarsound@hotmail.com>
2022-12-21 23:18:44 -0500

    BestScore

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int val = 0;
+        string ks = "";
+
+        if (myList.Count == 0)
+			return "None";
+
+        foreach(KeyValuePair<string, int> key in myList)
+        {
+            if (key.Value >= val)
+            {
+                val = key.Value;
+                ks = key.Key;
+            }
+        }
+        return ks;    
+    }
+}
