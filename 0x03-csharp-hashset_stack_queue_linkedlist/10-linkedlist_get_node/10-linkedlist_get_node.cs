25c4f7184f388cfd9415953f6f4f03d9b9ae3fa7
csarsound <csarsound@hotmail.com>
2022-12-23 21:09:08 -0500

    GetNodeAtIndex

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n > myLList.Count - 1 || n < 0)
+        {
+            return(0);
+        }
+        int con = 0;
+        foreach (int i in myLList)
+        {
+            if(con == n)
+            {
+                return(i);
+            }
+            else
+            {
+                con++;
+            }
+        }
+        return (0);
+
+    }
+}
