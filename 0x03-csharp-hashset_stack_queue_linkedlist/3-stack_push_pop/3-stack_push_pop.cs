06ff74a3fd33566c4c5fa21e9b1b446944009cdf
csarsound <csarsound@hotmail.com>
2022-12-23 20:21:57 -0500

    full stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count <= 0)
+        {
+            Console.WriteLine("Stack is empty");
+            Console.WriteLine("Stack contains \"{0}\": False", search);
+            aStack.Push(newItem);
+            return(aStack);
+        }
+        else
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        }
+        bool isIn = false;
+        foreach (string item in aStack)
+        {
+            if (item == search)
+            {
+                isIn = true;
+            }
+        }
+        if (isIn == false)
+        {
+            Console.WriteLine("Stack contains \"{0}\": False", search);
+            aStack.Push(newItem);
+            return(aStack);
+        }
+        Console.WriteLine("Stack contains \"{0}\": True", search);
+        while (aStack.Contains(search))
+        {
+            aStack.Pop();
+        }
+        aStack.Push(newItem);
+        return(aStack);
+
+    }
+}
