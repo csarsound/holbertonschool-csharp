1eea8416c1932554fceb61394dd5b80959a6f549
csarsound <csarsound@hotmail.com>
2022-12-23 19:41:27 -0500

    UniqueAddition

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        
+        HashSet<int> set = new HashSet<int>(myList);
+        int number = 0;
+        
+        foreach (int i in set)
+        {
+            number = number + i;
+        }
+        return (number);
+    }
+}
