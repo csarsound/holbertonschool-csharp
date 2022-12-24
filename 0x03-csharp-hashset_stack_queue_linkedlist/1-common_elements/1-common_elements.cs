6d753ed6b5adbcb281d72263290d1cb78a535baa
csarsound <csarsound@hotmail.com>
2022-12-23 19:59:27 -0500

    PresentInBoth

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> uniq2 = new HashSet<int>(list2);
+        HashSet<int> interator = new HashSet<int>(list1);
+        interator.IntersectWith(uniq2);
+        List<int> tot = new List<int>();
+        
+        foreach (var a in interator)
+        {
+            tot.Add(a);
+        }
+        return(tot);
+
+    }
+}
