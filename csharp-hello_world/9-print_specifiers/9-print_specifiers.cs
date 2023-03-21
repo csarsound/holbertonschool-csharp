6437c2dac74eef9ddd8cbbe5bfecedeba8235243
CesarSandino <csar_sound@hotmail.com>
2023-03-21 16:20:32 -0500

    PrintSpecifier

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@
             double percent = .7553;
             double currency = 98765.4321;
             Console.WriteLine("Percent: {0:0.00%}", percent);
-            Console.WriteLine("Currency: {0:C}", currently);
+            Console.WriteLine("Currency: {0:C}", currency);
         }
     }
