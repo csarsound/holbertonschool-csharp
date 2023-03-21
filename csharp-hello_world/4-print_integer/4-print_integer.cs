16d2906f974bd30a0d5c34e36f886913674282b9
CesarSandino <csar_sound@hotmail.com>
2023-03-21 14:49:33 -0500

    Print Integer

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 
-namespace _4_print_integer
-{
     class Program
     {
         static void Main(string[] args)
@@ -10,4 +8,3 @@ namespace _4_print_integer
             Console.WriteLine($"{number} Mission Street");
         }
     }
-}
