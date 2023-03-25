7cc7cf86a9ad2864bb5af85ad5a58da2d17918d1
CesarSandino <csar_sound@hotmail.com>
2023-03-25 09:25:32 -0500

    Hexadecimal

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (i = 0; i < 99; i++)
+            {
+                Console.Write("{0} = 0x{0:x}\n", i);
+            }
+        }
+    }
+}
