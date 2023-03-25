5b63436d16d86010268d5fd32d165853b9a35428
CesarSandino <csar_sound@hotmail.com>
2023-03-25 09:04:07 -0500

    lastdigit

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -13,11 +13,11 @@ namespace _1_last_digit
             {
                 Console.WriteLine($"The last digit of {number} is {lastdigit} and is greater than 5");
             }
-            else if (lasdigit == 0)
+            else if (lastdigit == 0)
             {
                 Console.WriteLine($"The last digit of {number} is {lastdigit} and is 0");
             }
-            else if (6 > lasdigit && lastdigit != 0)
+            else if (6 > lastdigit && lastdigit != 0)
             {
                 Console.WriteLine($"The last digit of {number} is {lastdigit} and is less than 6 and not 0");
             }
