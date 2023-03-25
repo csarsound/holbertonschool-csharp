c44de2d32f87588a17e7a56ebfc3b80626039571
CesarSandino <csar_sound@hotmail.com>
2023-03-25 09:01:16 -0500

    lastdigit

---
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1_last_digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndm = new Random();
+            int number = rndm.Next(-10000, 10000);
+            int lastdigit = (number % 10)
+            if (lastdigit > 5)
+            {
+                Console.WriteLine($"The last digit of {number} is {lastdigit} and is greater than 5");
+            }
+            else if (lasdigit == 0)
+            {
+                Console.WriteLine($"The last digit of {number} is {lastdigit} and is 0");
+            }
+            else if (6 > lasdigit && lastdigit != 0)
+            {
+                Console.WriteLine($"The last digit of {number} is {lastdigit} and is less than 6 and not 0");
+            }
+        }
+    }
+}
