f2ac568087d7819a99f71efaa866d00b2cdc6c94
CesarSandino <csar_sound@hotmail.com>
2023-03-25 09:34:56 -0500

    CountNumbers

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i <=99; i++)
+            {
+                if (i == 99)
+                {
+                    Console.Write("{0:00}\n", i);
+                }
+                else
+                {
+                    Console.Write("{0:00}, ", i);
+                }
+            }
+        }
+    }
+}
