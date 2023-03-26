6ea891b6289fd66118c46c9a10135e913bc5a869
CesarSandino <csar_sound@hotmail.com>
2023-03-25 21:04:25 -0500

    isLower

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
