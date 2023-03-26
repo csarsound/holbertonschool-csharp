64bce51e2d49be004bc474df590bcb694de6dd38
CesarSandino <csar_sound@hotmail.com>
2023-03-26 09:37:36 -0500

    LastDigit

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Program
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int num = number % 10;
+            if (num < 0)
+            {
+                num = num * -1;
+            }
+            Console.Write(num);
+            return num;
+        }
+    }
+}
