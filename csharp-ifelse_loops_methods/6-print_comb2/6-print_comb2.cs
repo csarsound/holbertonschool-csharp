4f83532028f41c689cc59426955095561144600a
CesarSandino <csar_sound@hotmail.com>
2023-03-25 20:24:43 -0500

    DifferentsNumbers

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0; i <= 9; i++)
+            {
+                for (j = i + 1; j <= 9; j++)
+                {
+                    if (i != j)
+                    {
+                        Console.Write($"{i}{j}");
+                    }
+                    if (!(i == 8 && j == 9))
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+        }
+    }
+}
