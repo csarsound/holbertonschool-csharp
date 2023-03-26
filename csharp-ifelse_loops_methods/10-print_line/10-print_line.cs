bb8a2975a4a031b3a03f43d7da4e9b078e8627a2
CesarSandino <csar_sound@hotmail.com>
2023-03-26 09:59:58 -0500

    StraightLine

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Program
+    {
+        public static void PrintLine(int lenght)
+        {
+           if (lenght > 0)
+           {
+            Console.Write(new String('_', lenght));
+           }
+           Console.WriteLine();
+        }
+    }
+}
