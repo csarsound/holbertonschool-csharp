18588c328ba7ce4232bd9891e188b81cb53f2b86
CesarSandino <csar_sound@hotmail.com>
2023-03-27 15:40:54 -0500

    ArrayOfIntegers

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,35 +1,33 @@
-﻿using System;
+﻿
 
-namespace _0_print_array
+using System;
+
+class Array
 {
-    class Array
-    {
-        public static int[] CreatePrint(int size)
-        {
-            int[] newArray;
+	public static int[] CreatePrint(int size)
+	{
+		int[] newArray;
 
-            if (size < 0)
-            {
-                Console.WriteLine("Size cannot be negative");
-                return (null);
-            }
+		if (size < 0)
+		{
+		Console.WriteLine("Size cannot be negative");
+		return (null);
+		}
 
-            newArray = new int[size];
+		newArray = new int[size];
 
-            for (int i = 0; i < size; i++)
-            {
-                newArray[i] = i;
-                if (i < size -1)
-                {
-                    Console.Write(newArray[i] + " ");
-                }
-                else
-                {
-                    Console.Write(newArray[i]);
-                }
-                Console.WriteLine();
-                return (newArray);
-            }
-        }
-    }
+		for (int i = 0; i < size; i++)
+		{
+		newArray[i] = i;
+		if (i < size - 1)
+			Console.Write(newArray[i] + " ");
+
+		else
+			Console.Write(newArray[i]);
+		}
+
+		Console.WriteLine();
+
+		return (newArray);
+	}
 }
