733cf02c1426b04c305edd291d61de099235bb62
CesarSandino <csar_sound@hotmail.com>
2023-03-28 17:22:28 -0500

    MaxInteger

--- csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
-    class List
+class List
+{
+    public static int MaxInteger(List<int> myList)
     {
-        public static int MaxInteger(List<int> myList)
+	int max;
+        if (myList.Count == 0)
         {
-            int max;
-            if (myList.Count == 0)
-            {
-                Console.WriteLine("List is empy");
-                return (-1);
-            }
-
-            max = myList[0];
-
-            foreach (var element in myList)
-            {
-                if (element > max)
-                {
-                    max = element;
-                }
-            }
-            return (max);
+            Console.WriteLine("List is empty");
+            return (-1);
         }
+
+        max = myList[0];
+        
+	foreach (var element in myList)
+	{
+		if (element > max)
+		{
+			max = element;
+		}
+	}
+	return (max);
     }
+}
