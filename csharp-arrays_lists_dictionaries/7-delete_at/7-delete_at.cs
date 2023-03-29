7f56bbc28e47e0e6c5d6f7f15d3c4c88fa2308fa
CesarSandino <csar_sound@hotmail.com>
2023-03-28 19:01:14 -0500

    Delete

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class List
+    {
+        public static List<int> DeleteAt(List<int> myList, int index)
+        {
+            if (index < 0 || index > myList.Count - 1)
+            {
+                Console.WriteLine("Index is out of range")
+                return (myList);
+            }
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (i == index)
+                {
+                    myList.Remove(myList[i]);
+                    break;
+                }
+            }
+            return (myList);
+        }
+    }
