6bd46b79894f39445804c987c89fbd7899ac9117
CesarSandino <csar_sound@hotmail.com>
2023-03-29 16:17:18 -0500

    AddKey

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/ 9-add_key_value.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int countKeys = 0;
+        foreach (keyValuePair<string, string> pair in myDict)
+        {
+            CountKeys++;
+        }
+        return countKeys;
+    }
+}
