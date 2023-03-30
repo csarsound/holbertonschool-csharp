937c3092272043154e07ad6843614e8affa589c3
CesarSandino <csar_sound@hotmail.com>
2023-03-29 19:18:28 -0500

    BestScore

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void BestScore(Dictionary<string, int> myList)
+    {
+        KeyValuePair<string, int> bestScore;
+
+        if (myList.count == 0 || myList == null)
+            return ("None");
+
+        bestScore = myList.First();
+        foreach (KeyValuePair<string, int> element in myList)
+        {
+            if (element.Value > bestScore.Value)
+            {
+                bestScore = element;
+            }
+        }
+        return (bestScore.Key);
+    }
+}
