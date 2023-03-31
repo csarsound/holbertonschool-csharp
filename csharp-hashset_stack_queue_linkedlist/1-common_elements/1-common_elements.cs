8f599a1c20d9385a4326308df42f7fb482c65ed4
CesarSandino <csar_sound@hotmail.com>
2023-03-30 21:08:50 -0500

    commonElements

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collection.Generic;
+
+namespace _1_common_elements
+{
+    class List
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {
+            List<int> commonElemenList = new List<int>();
+
+            if (list1.Count != 0 && list2.Count != 0)
+            {
+                list1.Sort();
+                list2.Sort();
+
+                foreach (var element1 in list1)
+                {
+                    foreach (var element2 in list2)
+                    {
+                        if (element2 == element1)
+                        {
+                            commonElementList.Add(element2);
+                            break;
+                        }
+                    }
+                }
+            }
+            return (commonElementList);
+        }
+    }
+}
