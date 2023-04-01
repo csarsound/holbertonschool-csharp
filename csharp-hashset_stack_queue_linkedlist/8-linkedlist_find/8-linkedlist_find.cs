548774025a90e2277acd51cf59e6bf7facd3800b
CesarSandino <csar_sound@hotmail.com>
2023-03-31 20:37:31 -0500

    FindNode

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myList, int value)
+    {
+        int index = 0;
+
+        if (!myLList.Contains(value))
+            return (-1);
+
+        foreach (var item in myLList)
+        {
+            if (item == value)
+                break;
+
+            index++;
+        }
+        return (index);
+    }
+}
