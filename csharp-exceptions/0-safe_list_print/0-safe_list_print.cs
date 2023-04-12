d55e8ef87c8e5ced5d2a5d820b3e04f87b0d4ad5
CesarSandino <csar_sound@hotmail.com>
2023-04-11 19:47:58 -0500

    ListExceptions

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+
+        for (int i = 0; i < n; i++, count++)
+        {
+            try
+            {
+                Console.WriteLine(myList[i]);
+            }
+            catch (System.Exception)
+            {
+                break;
+            }
+        }
+
+        return (count);
+    }
+}
