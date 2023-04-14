8e5e4c8ea00e4cd242389ce93ab888cfcda5a4d8
CesarSandino <csar_sound@hotmail.com>
2023-04-14 15:03:39 -0500

    OverrideToString

---
+++ csharp-structs_enums/3-dog/3-main.cs
@@ -0,0 +1,10 @@
+{
+    static void Main(string[] args)
+    {
+        Dog poppy;
+
+        poppy = new Dog("Poppy", 0.7f, "Kris", Rating.Excellent);
+
+        Console.WriteLine(poppy.ToString());
+    }
+}
