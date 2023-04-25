3721a8311330d8fcbd3d57db6a17c195e50f6b9a
Cesar Daniel Martinez Sandino <csar@MacBook-Pro-de-Cesar.local>
2023-04-25 14:56:08 -0500

    JustSubclass

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>Provides some methods to work with Obj</summary>
+class Obj
+{
+    /// <summary>Write a method that print true Only subclass
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubcclassof(baseType);
+    }
+}
