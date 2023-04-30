56601bb48405fdac751e7978df6ca94ec8a49f61
Cesar Daniel Martinez Sandino <csar@MacBook-Pro-de-Cesar.local>
2023-04-30 15:00:34 -0500

    toString

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,53 @@
+﻿namespace Enemies
+{
+    /// <summary>Create an empty public class Zombie within Enemies that defines a zombie</summary>
+    public class Zombie
+    {
+        /// <summary>Int that represents the value of the Zombie health</summary>
+        private int health;
+
+        /// <summary>Private field that sould be a string</summary>
+        private string name = "(No name)";
+
+        /// <summary>String that represents the name of the Zombie</summary>
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/>class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/>class that value must be greater than or equal to 0</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+
+            else
+                this.health = value;
+        }
+
+        /// <summary>public method that return the health of the Zombie object</summary>
+        public int GetHealth()
+        {
+            return (this.health);
+        }
+
+        /// <summary>public .toString() that print the Zombie object´s attributes to stdout</summary>
+        public override string ToString()
+        {
+            return ($"Zombie Name: {this.name} / Total Health: {this.health}");
+        }
+    }
+}
