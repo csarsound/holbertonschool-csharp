98e25d07bb630194c24286b0864e44c6a1cbec80
Cesar Daniel Martinez Sandino <csar@MacBook-Pro-de-Cesar.local>
2023-05-28 19:27:25 -0500

    InvertColor

--- image_processor/main.cs
+++ image_processor/main.cs
@@ -1,17 +1,39 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    string[] filenames = Directory.GetFiles("images/", "*.jpg");
+
+    if (filenames.Length <= 10)
     {
-        string[] filenames;
-
-        if (args.Length > 1)
-            filenames = args;
-        else
-            filenames = Directory.GetFiles("images/", "*.jpg");
-
-        ImageProcessor.Inverse(filenames);
+      foreach (string file in filenames)
+      {
+        for (int i = 1; i <=5; i++)
+          File.Copy(file, $"{file.Split('.')[0]}_{i}.jpg");
+      }
     }
+
+    filenames = Directory.GetFiles("images/", "*.jpg");
+
+    TimeSpan timespan;
+    TimeSpan limit = new TimeSpan(0, 0, 0, 40, 0);
+    Stopwatch timer = Stopwatch.StartNew();
+
+    ImageProcessor.Inverse(filenames);
+
+    timer.Stop();
+    timespan = timer.Elapsed;
+
+    if (timespan < limit)
+      Console.WriteLine("OK");
+    else
+      Console.WriteLine($"Runtime too long: {timespan}");
+
+    foreach (string file in Directory.GetFiles(".", "*.jpg"))
+      File.Delete(file);
+  }
 }
