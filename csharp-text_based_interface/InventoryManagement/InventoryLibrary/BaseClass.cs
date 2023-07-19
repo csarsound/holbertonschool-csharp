a0e2c5aae6dd85daa90dc438cc038bf7f53e064f
Cesar Daniel Martinez Sandino <csar@MacBook-Pro-de-Cesar.local>
2023-07-19 18:52:25 -0500

    LibraryClass

---
+++ csharp-text_based_interface/InventoryManagement/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace InventoryLibrary
+{
+    public class MyClass
+    {
+        public class BaseClass
+        {
+            public string id { get; set; }
+            public DateTime date_created { get; set; }
+            public DateTime date_updated { get; set; }
+        }
+    }
+}
