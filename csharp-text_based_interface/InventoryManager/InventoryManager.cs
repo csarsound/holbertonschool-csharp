7296442df42b02c723c90ee5111d4dda6ce4259f
Cesar Daniel Martinez Sandino <csar@MacBook-Pro-de-Cesar.local>
2023-07-20 13:31:15 -0500

    NUnitFramework

--- csharp-text_based_interface/InventoryManager/InventoryManager.cs
+++ csharp-text_based_interface/InventoryManager/InventoryManager.cs
@@ -1,88 +1,105 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
-namespace InventoryManager
+
+public class Program
 {
-    public class Program
+    private delegate int inputMethod(string[] input);
+    private static Dictionary<string, inputMethod> methods;
+    private static Dictionary<string, string> classes;
+    private static readonly JSONStorage<BaseClass> jsonStorage = new JSONStorage<BaseClass>();
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Command to execute
+        string cmdExe = null;
+        // Commands array to execute
+        string[] cmdSExe = null;
+        jsonStorage.Load();
+        Initialize();
+        ShowInitialPrompt();
+
+        while (true)
         {
-            JSONStorage storage = new JSONStorage();
-            storage.Cargar();
+            ShowPromt();
+            // Catch the commands typed
+            cmdExe = Console.ReadLine();
+            cmdSExe = ParseCommand(cmdExe);
 
-            Console.WriteLine("Inventory Manager");
-            Console.WriteLine("-------------------------");
+            // Check 
+            if(cmdSExe.Lenght <= 0) { continue; }
 
-            while (true)
+            string cmd = cmdExe[0].ToLower();
+            if(cmd == "exit") { break; }
+
+            if(methods.ContainsKey(cmd))
             {
-                Console.WriteLine("\nComandos disponibles:");
-                Console.WriteLine("<ClassNames> show all ClassNames of objects");
-                Console.WriteLine("<All> show all objects");
-                Console.WriteLine("<All [ClassName]> show all objects of a ClassName");
-                Console.WriteLine("<Create [ClassName]> a new object");
-                Console.WriteLine("<Show [ClassName object_id]> an object");
-                Console.WriteLine("<Update [ClassName object_id]> an object");
-                Console.WriteLine("<Delete [ClassName object_id]> an object");
-                Console.WriteLine("<Exit> Quit the application");
-
-                Console.Write("\nIngrese un comando: ");
-                string input = Console.ReadLine().Trim();
-                string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-                if (parts.Length == 0)
-                {
-                    Console.WriteLine("Comando inválido. Intente nuevamente.");
-                    continue;
-                }
-
-                string command = parts[0].ToLower();
-                if (command == "exit")
-                {
-                    storage.Guardar();
-                    break;
-                }
-
-                switch (command)
-                {
-                    case "classnames":
-                        storage.ClassNames();
-                        break;
-                    case "all":
-                        if (parts.Length == 1)
-                            storage.All();
-                        else if (parts.Length == 2)
-                            storage.All(parts[1]);
-                        else
-                            Console.WriteLine("Comando inválido. Intente nuevamente.");
-                        break;
-                    case "create":
-                        if (parts.Length == 2)
-                            storage.Create(parts[1]);
-                        else
-                            Console.WriteLine("Comando inválido. Intente nuevamente.");
-                        break;
-                    case "show":
-                        if (parts.Length == 3)
-                            storage.Show(parts[1], parts[2]);
-                        else
-                            Console.WriteLine("Comando inválido. Intente nuevamente.");
-                        break;
-                    case "update":
-                        if (parts.Length == 3)
-                            storage.Update(parts[1], parts[2]);
-                        else
-                            Console.WriteLine("Comando inválido. Intente nuevamente.");
-                        break;
-                    case "delete":
-                        if (parts.Length == 3)
-                            storage.Delete(parts[1], parts[2]);
-                        else
-                            Console.WriteLine("Comando inválido. Intente nuevamente.");
-                        break;
-                    default:
-                        Console.WriteLine("Comando inválido. Intente nuevamente.");
-                        break;
-                }
+                if( methods[cmd](cmdSExe) == 0 ) { ShowInitialPrompt(); }
+            }
+            else
+            {
+                Console.WriteLine($"{cmdSExe[0]} command to execute not found");
             }
         }
+        Console.WriteLine("\nBYE. See you Later\n");
+    }
+
+    /// <sumary> Method to print Prompt </sumary>
+    private static void Showprompt()
+    {
+        string prompt = "CONSOLE >> ";
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write($"{prompt}");
+        Console.ReserColor();
+    }
+
+    /// <sumary> Method to print the initial prompt </sumary>
+    private static void ShowInitialprompt()
+    {
+        StringBuilder message = new StringBuilder();
+        message.Append("\n********************************************\n");
+        message.Append("\t\tInventory Manager");
+        message.Append("\n********************************************\n");
+        message.Append("<ClassNames> Show all ClassNames of objects\n");
+        message.Append("<All> Show all objects\n");
+        message.Append("<All [ClassName]> Show all objects of a ClassName\n");
+        message.Append("<Create [ClassName]> Create a new object\n");
+        message.Append("<Show [ClassName object_id]> Show an object\n");
+        message.Append("<Update [ClassName object_id]> Update an object\n");
+        message.Append("<Delete [ClassName object_id]> Delete an object\n");
+        message.Append("<Exit> exit");
+        // Print the message
+        Console.WriteLine(message);
+    }
+
+    /// <sumary> Method to parse the input commands </sumary>
+    private static string[] ParseCommand(string comExe)
+    {
+        return comExe.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    /// <sumary>  </sumary>
+    private static void Initialize()
+    {
+        methods = new Dictionary<string, method>()
+        {
+            {"classnames", ClassNames},
+            {"all", All},
+            {"create", Create},
+            {"show", Show},
+            {"update", Update},
+            {"delete", Delete}
+        };
+
+        classes = new Dictionary<string, string>()
+        {
+            {"baseclass", "BaseClass"},
+            {"item", "Item"},
+            {"user", "User"},
+            {"inventory", "Inventory"}
+        };
     }
 }
